52856234e9eff2099f6de25235f8cb1a015cebb6
Lucas de Castro <lucas.castro.souza@al.infnet.edu.br>
2025-08-17 14:27:43 -0300

    Add Home and Settings Views

---
+++ Bookry/Bookry/Modules/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Bookry
+//
+//  Created by Lucas de Castro Souza on 15/08/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
